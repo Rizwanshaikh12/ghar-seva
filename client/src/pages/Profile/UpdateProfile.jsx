e0bd875ead1461bf5760f83807b3dc196e8f1303
rizwan <rizwan3786shaikh@gmail.com>
2024-05-31 00:56:22 +0530

    final commit 3

--- client/src/pages/Profile/UpdateProfile.jsx
+++ client/src/pages/Profile/UpdateProfile.jsx
@@ -75,7 +75,7 @@ const UpdateProfile = () => {
             }
 
             await toast.promise(
-                axios.put(`${process.env.REACT_APP_SERVER_BASE_URL}/api/user/${id}`, {
+                axios.put(`${process.env.REACT_APP_SERVER_BASE_URL}/user/${id}`, {
                     city, name, email, password, address, mobile, profileImage: file
                 }, {
                     headers: {
